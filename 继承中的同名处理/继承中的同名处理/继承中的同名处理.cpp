0179cf48b36a0be0a5802abb3a3cf8bcaf928f2f
gao <gao@qq.com>
2020-05-05 23:25:22 +0800

    same name handle under inherit

---
+++ 继承中的同名处理/继承中的同名处理/继承中的同名处理.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>  
+using namespace std;
+
+class Base 
+{
+public:
+    Base() 
+    {
+        m_A = 100;
+    }
+    void func()
+    {
+        cout << "Base中func调用" << endl;
+    }
+
+
+    int  m_A;
+};
+
+class Son :public Base
+{
+public:
+    Son()
+    {
+        m_A = 200;
+    }
+    void func()
+    {
+        cout << "Son中func调用" << endl;
+    }
+
+    int m_A;
+};
+
+void test01()
+{
+    Son s1;
+    cout << s1.m_A << endl;     //就近原则
+    //若要调用父类中的m_A;
+    cout << s1.Base::m_A << endl;       //加作用域
+    
+    s1.func();
+    //父类func调用
+    s1.Base::func();
+}
+//子类父类拥有同名的函数、属性，那么子类父类同名的函数、属性同时存在，调用时就近原则
+//子类会把父类同名函数都隐藏，需要加作用域
+
+int main()
+{
+    test01();
+    system("pause");
+    return EXIT_SUCCESS;
+
+}
